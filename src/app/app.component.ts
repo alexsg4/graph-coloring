914a0c2c649323102430ef7e534b6fac99ce3d78
Alexandru Stanciu <stanciu.alex74@gmail.com>
2019-11-21 23:59:07 +0200

    Initial commit: render a graph with sigma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import sigma from 'sigma';
+
+declare const sigma: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'graph-coloring';
+
+  ngOnInit(): void {
+    sigma.parsers.gexf(
+      'assets/graphs/testGraph.gexf',
+      { container: 'graph-container' }
+    );
+  }
 }
