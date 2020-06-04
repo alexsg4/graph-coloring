0213836bf4b6f969f71404d635e7816c53cd17ec
Alexandru Stanciu <stanciu.alex74@gmail.com>
2020-06-04 12:03:39 +0300

    add docstring

--- src/app/graph/coloring-strategy/coloring-solution.ts
+++ src/app/graph/coloring-strategy/coloring-solution.ts
@@ -1,18 +1,28 @@
-import { isNullOrUndefined } from 'util';
-
 export class ColoringSolution {
   coloring: Map<string, number>;
   numColors: number;
   numConfChecks: number;
 
+  /**
+   * Class for storing a coloring solution for a graph
+   *
+   * @param coloring - a map assigning a color id to a single node's id in the graph
+   * @param numColors - the number of color classes used
+   * @param numChecks - the number of clash checks performed when building the solution
+   */
   constructor(coloring: Map<string, number>, numColors: number, numChecks = 0) {
     this.coloring = coloring;
     this.numColors = numColors;
     this.numConfChecks = numChecks;
   }
 
+  /**
+   * Checks if the solution is valid (i.e. no 2 adjacent nodes have the same color)
+   *
+   * @param graph - the graph we operate on
+   */
   public isValid(graph: any): boolean {
-    if (isNullOrUndefined(this.coloring)) {
+    if (this.coloring === null || this.coloring === undefined) {
       console.error('isSolutionValid: coloring is null or undefined.');
       return undefined;
     }
